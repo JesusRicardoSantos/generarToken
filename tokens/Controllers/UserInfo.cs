a6d11aa3834fee7e5777d3cade4614ea84fdec32
jose de jesus ricardo santos <jose.jesus.ricardo.santos@outlook.com>
2018-09-05 14:51:21 -0500

    Hola

---
+++ tokens/Controllers/UserInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace tokens.Controllers
+{
+    public class UserInfo
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+    }
+}
